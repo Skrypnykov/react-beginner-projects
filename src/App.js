974fa2d82f1ef42425e1442a077ead5e00952b4d
Skrypnykov <oskripnikov@gmail.com>
2023-10-10 15:42:53 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ function App() {
   const [count, setCount] = React.useState(0);
 
   const onClickPlus = () => {
-    setCount(count +1);
+    setCount(count + 1);
   }
 
   const onClickMinus = () => {
-    setCount(count -1);
+    setCount(count - 1);
   }
-  
   
   return (
     <div className="App">
