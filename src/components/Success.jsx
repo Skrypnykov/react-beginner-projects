54ac59bdf31a90c823640e7d73661d51bbcddd54
Skrypnykov <oskripnikov@gmail.com>
2023-10-16 14:51:07 +0300

    users refactoring

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Success = ({ count }) => {
   return (
-    <div class="success-block">
+    <div className="success-block">
       <img src="/assets/success.svg" alt="Success" />
       <h3>Успішно!</h3>
       <p>Всім {count} користувачам надіслано запрошення.</p>
